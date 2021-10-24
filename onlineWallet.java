f1890ea90f713678d8b3d48f3540ce11a62e93fd
Cyndril10 <93090853+Cyndril10@users.noreply.github.com>
2021-10-24 22:07:04 +0530

    Add files via upload

---
+++ onlineWallet.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+public class onlineWallet extends Menu {
+
+	
+			public static void main(String[] args) throws IOException {
+				Menu m = new Menu();
+
+				m.getLogin();
+			}
+		
+
+}
